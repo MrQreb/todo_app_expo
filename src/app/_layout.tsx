a4f391fc3e136f73c228c739749cf668c794941a
Jesus Eduardo Arias Diaz <devmr.qreb@outlook.com>
2026-03-17 08:46:56 -0600

    drawer removed

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -5,11 +5,11 @@ import { ToastProvider } from '@src/components/ui/Toast';
 import { db } from '@src/db/client';
 import { useMediaLibraryPermission } from '@src/hooks/useMediaLibraryPermission';
 import { useMigrations } from 'drizzle-orm/expo-sqlite/migrator';
-import { Drawer } from 'expo-router/drawer';
 import { StatusBar } from 'expo-status-bar';
 import { Text, View } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import 'react-native-reanimated';
+import HomeScreen from '../screen/home-screen';
 
 export default function RootLayout() {
   const { success, error, } = useMigrations(db, migrations);
@@ -31,31 +31,7 @@ export default function RootLayout() {
     <ThemeProvider value={DarkTheme}>
       <GestureHandlerRootView style={{ flex: 1 }}>{/* Necesary to use gestures in toast */}
         <ToastProvider>
-          <Drawer
-            screenOptions={{
-              drawerStyle: {
-                backgroundColor: '#1C1C1E',
-              },
-              headerStyle: {
-                backgroundColor: '#141414',
-              },
-              headerShadowVisible: false,
-              drawerActiveTintColor: '#F2F2F7',
-              drawerInactiveTintColor: 'rgba(255, 255, 255, 0.4)',
-            }}
-          >
-            <Drawer.Screen
-              name="index"
-              options={{
-                drawerLabel: 'Tareas',
-                title: '',
-                headerStyle: {
-                  backgroundColor: '#030000',
-                },
-              }}
-
-            />
-          </Drawer>
+          <HomeScreen/>
           <StatusBar style="auto" />
         </ToastProvider>
       </GestureHandlerRootView>
